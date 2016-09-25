709998b16c1c7d3b9a7f295db52bee697b9a341e
tilhari95 <suyash.tilhari12@gmail.com>
2016-09-25 22:36:30 +0530

    added:configuraton for light admin portal

---
+++ src/org/lightadmin/config/RegistrationAdministration.java
@@ -0,0 +1,14 @@
+package org.lightadmin.config;
+
+import org.gdgjss.model.Registration;
+import org.lightadmin.api.config.AdministrationConfiguration;
+
+public class RegistrationAdministration {
+	@SuppressWarnings("unused")
+	public class UserAdministration extends AdministrationConfiguration<Registration> {
+
+
+	    
+	}
+
+}
